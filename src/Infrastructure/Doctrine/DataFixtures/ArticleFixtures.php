507ef00390b6843d8bc5521571bfc67c8a4c4406
Gino <rakotozafy@ext.lemonde.fr>
2024-03-13 10:45:48 +0100

    User Entity

--- src/Infrastructure/Doctrine/DataFixtures/ArticleFixtures.php
+++ src/Infrastructure/Doctrine/DataFixtures/ArticleFixtures.php
@@ -2,9 +2,11 @@
 
 namespace App\Infrastructure\Doctrine\DataFixtures;
 
+use App\Infrastructure\Doctrine\Entity\UserDoctrine;
 use App\Infrastructure\Doctrine\Factory\ArticleDoctrineFactory;
 use App\Infrastructure\Doctrine\Factory\CategoryDoctrineFactory;
 use App\Infrastructure\Doctrine\Factory\TagDoctrineFactory;
+use App\Infrastructure\Doctrine\Factory\UserDoctrineFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -13,6 +15,7 @@ class ArticleFixtures extends Fixture implements FixtureGroupInterface
 {
     public function load(ObjectManager $manager): void
     {
+        UserDoctrineFactory::createMany(10);
         TagDoctrineFactory::createMany(20);
         CategoryDoctrineFactory::createMany(5);
         ArticleDoctrineFactory::createMany(100);
