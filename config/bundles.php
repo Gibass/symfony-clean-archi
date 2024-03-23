6c9f54afee311c4fe91e7f6ef08ba8c80e70d0dc
Gino <rakotozafy@ext.lemonde.fr>
2024-03-23 09:55:38 +0100

    Remove Bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
